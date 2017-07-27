6b514a64bc4840251e1feade70f329f6115c48af
alexsmithkarlberg <alex.smith.karlberg.91@gmail.com>
2017-07-27 11:08:47 +0200

    Made all classes and started on Queen-positioning method.

---
+++ Schack-Matt/Schack-Matt/Positions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schack_Matt
+{
+    public class Positions
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+    }
+}
---
+++ Schack-Matt/Schack-Matt/Queen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schack_Matt
+{
+    class Queen
+    {
+        public List<Positions> GetAllMoves(Positions currentPos)
+        {
+            List<Positions> moves = new List<Positions>();
+      
+
+            // This part will add all positions along the +y axis
+
+            for (int i = 0; i < 7; i++)
+            {
+                Positions pos = new Positions();
+                pos.PosX = currentPos.PosX;
+                pos.PosY = currentPos.PosY + 1;
+                moves.Add(pos);
+            }
+
+            // This part will add all positions along the +x axis
+
+            for (int i = 0; i < 7; i++)
+            {
+                Positions pos = new Positions();
+                pos.PosX = currentPos.PosX + 1;
+                pos.PosY = currentPos.PosY;
+                moves.Add(pos);
+            }
+
+            // This part will add all positions along the +x, +y axis
+
+            for (int i = 0; i < 7; i++)
+            {
+                Positions pos = new Positions();
+                pos.PosX = currentPos.PosX + 1;
+                pos.PosY = currentPos.PosY + 1;
+                moves.Add(pos);
+            }
+
+            // This part will add all positions along the +x, -y axis
+
+            for (int i = 0; i < 7; i++)
+            {
+                Positions pos = new Positions();
+                pos.PosX = currentPos.PosX + 1;
+                pos.PosY = currentPos.PosY - 1 ;
+                moves.Add(pos);
+            }
+
+            // This part will add all positions along the -x, +y axis
+
+            for (int i = 0; i < 7; i++)
+            {
+                Positions pos = new Positions();
+                pos.PosX = currentPos.PosX - 1;
+                pos.PosY = currentPos.PosY + 1;
+                moves.Add(pos);
+            }
+
+        }
+    }
+}
